476c5011ef45dc25ceb69883adfc85aa521dbaef
leathebest <noamlea74@gmail.com>
2021-01-20 12:05:45 +0200

    pulled from ryan branch and added api.js , userReducer and authContext

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,28 @@
+import Cookies from 'js-cookie'
+
+export const initialState = {
+    isAuthnticated: false,
+    userId: Cookies.get('uid') || null 
+};
+
+export const userReducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            Cookies.set('uid', action.payload.uid)
+            return {
+                ...state,
+                isAuthnticated: true,
+                userId: action.payload.uid,
+            };
+        case 'LOGOUT':
+            Cookies.remove('uid');
+            return {
+                ...state,
+                isAuthnticated: false,
+                userId: null,
+                token: null
+            };
+        default:
+            return state;
+    }
+}
