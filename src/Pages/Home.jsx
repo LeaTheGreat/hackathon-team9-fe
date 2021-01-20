f63626840c27c13633b1b207927ca8731bea7f74
Violetta Kegeles <46904779+vkegeles@users.noreply.github.com>
2021-01-21 01:58:17 +0200

    change articles at home page, use material-ui component instead of custom

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import Baby from '../scss/img/landing-img.jpg';
-import Article1 from '../scss/img/article-1.jpg';
-import Article2 from '../scss/img/article-2.jpg';
-import Article3 from '../scss/img/article-3.jpg';
-import { FaArrowCircleRight } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import Navbar from '../Components/Navbar';
+import React, { useState, useEffect, useCallback } from "react";
+import Baby from "../scss/img/landing-img.jpg";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import { getArticles } from "../lib/api";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles({
+  media: {
+    height: 200,
+  },
+});
 
 const Home = () => {
   return (
@@ -21,73 +36,78 @@ const Home = () => {
         <div className="test">
           <h3 className="test__heading">Test your child</h3>
           <p className="test__description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia rem deserunt optio omnis
-            ipsa tempora.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia rem
+            deserunt optio omnis ipsa tempora.
           </p>
           <button className="test__button">TEST</button>
         </div>
         <div className="test">
           <h3 className="test__heading">Latest Articles</h3>
           <p className="test__description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia rem deserunt optio omnis
-            ipsa tempora.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia rem
+            deserunt optio omnis ipsa tempora.
           </p>
         </div>
       </section>
 
-      {/* ARTICLE SECTION */}
-      <section className="articles">
-        <article className="article">
-          <img src={Article1} alt="article-1" className="article__img" />
-          <h4 className="article__title">Article Title</h4>
-          <p className="article__description">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio magnam dignissimos libero
-            commodi repellat deserunt, quam inventore unde ratione est perferendis id.
-          </p>
-          <a
-            href="http://www.linkedin.com"
-            target="_blank"
-            rel="noreferrer"
-            className="article__btn"
-          >
-            <FaArrowCircleRight />
-          </a>
-        </article>
-        <article className="article">
-          <img src={Article2} alt="article-2" className="article__img" />
-          <h4 className="article__title">Article Title</h4>
-          <p className="article__description">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio magnam dignissimos libero
-            commodi repellat deserunt, quam inventore unde ratione est perferendis id.
-          </p>
-          <a
-            href="http://www.linkedin.com"
-            target="_blank"
-            rel="noreferrer"
-            className="article__btn"
-          >
-            <FaArrowCircleRight />
-          </a>
-        </article>
-        <article className="article">
-          <img src={Article3} alt="article-3" className="article__img" />
-          <h4 className="article__title">Article Title</h4>
-          <p className="article__description">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio magnam dignissimos libero
-            commodi repellat deserunt, quam inventore unde ratione est perferendis id.
-          </p>
-          <a
-            href="http://www.linkedin.com"
-            target="_blank"
-            rel="noreferrer"
-            className="article__btn"
-          >
-            <FaArrowCircleRight />
-          </a>
-        </article>
-      </section>
+      <Container maxWidth="lg" className="container">
+        <ArticleContainer />
+      </Container>
     </>
   );
 };
+
+function ArticleContainer(props) {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getArticles().then((res) => {
+      console.log(res);
+      setArticles(res);
+    });
+  }, []);
+  return (
+    // <section className="articles">
+    <Grid container spacing={4}>
+      {articles &&
+        articles.map((article) => (
+          <Article key={article._id} article={article} />
+        ))}
+    </Grid>
+  );
+}
+
+function Article({ article }) {
+  const history = useHistory();
+  const classes = useStyles();
+
+  const callback = useCallback(() => history.push(`/article/${article._id}`), [
+    history,
+    article._id,
+  ]);
+
+  return (
+    <Grid item xs={12} md={4} justify="space-evenly">
+      <Card className={classes.root} onClick={callback}>
+        <CardActionArea>
+          <CardMedia className={classes.media} image={article.picture} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {article.caption}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {article.shorttext}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+}
 
 export default Home;
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,51 +1,76 @@
-import axios from 'axios';
+import axios from "axios";
 
 const baseUrl = "http://localhost:5000";
 
 const singup = async (user) => {
-    try {
-        const response = await axios.post(baseUrl + '/api/users/signup', { user });
-        return response.data;
-    } catch (err) {
-        return err;
-    }
-}
+  try {
+    const response = await axios.post(baseUrl + "/api/users/signup", { user });
+    return response.data;
+  } catch (err) {
+    return err;
+  }
+};
 const login = async (user) => {
-    try {
-        const response = await axios.post(baseUrl + '/api/users/login', { user });
-        return response.data;
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const response = await axios.post(baseUrl + "/api/users/login", { user });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const logout = async () => {
-    try {
-        const response = await axios.post(baseUrl + '/api/users/logout');
-        return response.data;
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const response = await axios.post(baseUrl + "/api/users/logout");
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getUserById = async (id) => {
-    try {
-        const response = await axios.get(baseUrl + `/api/users/${id}`);
-        return response.data;
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const response = await axios.get(baseUrl + `/api/users/${id}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const updateUser = async (id, userInfo) => {
-    try {
-        const response = await axios.put(baseUrl + `/api/users/${id}`, { userInfo });
-        return response.data;
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const response = await axios.put(baseUrl + `/api/users/${id}`, {
+      userInfo,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getArticles = async () => {
+  try {
+    const response = await axios.get(baseUrl + `/api/article?limit=3`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const getArticleById = async (id) => {
+  try {
+    const response = await axios.get(baseUrl + `/api/article/${id}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export {
-    singup, login, getUserById, updateUser, logout
+  singup,
+  login,
+  getUserById,
+  updateUser,
+  logout,
+  getArticles,
+  getArticleById,
 };
