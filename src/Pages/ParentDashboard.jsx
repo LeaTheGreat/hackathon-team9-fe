e5b7601698db877c9d9b9c0d6c2c0cad474c514b
Violetta Kegeles <46904779+vkegeles@users.noreply.github.com>
2021-01-21 14:03:36 +0200

    change of parrent dashboard

--- src/Pages/ParentDashboard.jsx
+++ src/Pages/ParentDashboard.jsx
@@ -1,70 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import DeleteIcon from '@material-ui/icons/Delete';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import NavbarFull from '../Components/NavbarFull';
-import { Button, Toolbar, Typography } from '@material-ui/core';
-import AddChildModal from '../Modals/AddChild';
-import { getChildrenRelatedToParent } from '../lib/api';
-import { useAuth } from '../context/AuthContext';
+import React, { useEffect, useState, useCallback } from "react";
+import { useHistory } from "react-router-dom";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import NavbarFull from "../Components/NavbarFull";
+import { Button, Toolbar, Typography } from "@material-ui/core";
+import AddChildModal from "../Modals/AddChild";
+import { getChildrenRelatedToParent } from "../lib/api";
+import { useAuth } from "../context/AuthContext";
 
 const useStyles = makeStyles({
   root: {
-    width: '95%',
-    margin: 'auto',
+    width: "95%",
+    margin: "auto",
   },
   container: {
-    borderRadius: '20px',
+    borderRadius: "20px",
     maxHeight: 440,
   },
   toolbar: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    display: "flex",
+    justifyContent: "space-between",
   },
 });
 
 const columns = [
-  { id: 'child', label: 'Child', minWidth: 170 },
-  { id: 'phone', label: 'Phone Number', minWidth: 150 },
-  { id: 'status', label: 'Status', minWidth: 50 },
-  { id: 'actions', label: 'Actions', minWidth: 125 },
+  { id: "name", label: "Child", minWidth: 170 },
+  { id: "doctor", label: "Doctor", minWidth: 100 },
+  { id: "status", label: "Status", minWidth: 50 },
+  { id: "actions", label: "Actions", minWidth: 100 },
 ];
 
-function createData(child, phone, status, actions) {
-  return { child, phone, status, actions };
-}
+const actionButtons = <div></div>;
 
-const actionButtons = (
-  <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-    <Button variant="contained" color="primary">
-      View
-    </Button>
-    <Button variant="contained" color="secondary" startIcon={<DeleteIcon />}>
-      Delete
-    </Button>
-  </div>
-);
-
-const rows = [
-  createData('Ryan', 94712430342, 'Tested', actionButtons),
-  createData('Ryan', 94712430342, 'Awaiting Results', actionButtons),
-  createData('Ryan', 94712430342, 'Tested', actionButtons),
-];
+// const rows = [
+//   createData("Ryan", 94712430342, "Tested", actionButtons),
+//   createData("Ryan", 94712430342, "Awaiting Results", actionButtons),
+//   createData("Ryan", 94712430342, "Tested", actionButtons),
+// ];
 
 export default function StickyHeadTable() {
   const { auth } = useAuth();
-
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [open, setOpen] = useState(false);
+  const [rows, setRows] = useState();
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -76,13 +66,14 @@ export default function StickyHeadTable() {
   };
 
   useEffect(() => {
-    console.log('USER ID: ', auth.userId);
     getChildrenRelatedToParent(auth.userId).then((res) => {
-      console.log('CHILDREN!!!!!', res);
+      console.log(res);
+      const children = res.map((child) => {
+        return { ...child, actions: actionButtons };
+      });
+      setRows(children);
     });
-  }, []);
-
-  const [open, setOpen] = useState(false);
+  }, [auth.userId]);
 
   const toggleModal = () => {
     setOpen(!open);
@@ -121,36 +112,81 @@ export default function StickyHeadTable() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
+              {rows &&
+                rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <ChildRow key={row._id} row={row} columns={columns} />
+                    );
+                  })}
             </TableBody>
           </Table>
         </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onChangePage={handleChangePage}
-          onChangeRowsPerPage={handleChangeRowsPerPage}
-        />
+        {rows && (
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        )}
       </Paper>
       <AddChildModal open={open} toggleModal={toggleModal} />
     </div>
+  );
+}
+
+function ChildRow({ row, columns }) {
+  const history = useHistory();
+  console.log(row);
+  const callback = useCallback(() => history.push(`/survey/${row._id}`), [
+    history,
+    row._id,
+  ]);
+
+  return (
+    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+      {columns.map((column) => {
+        if (column.id != "actions") {
+          const value = row[column.id];
+          return (
+            <TableCell key={column.id} align={column.align}>
+              {column.format && typeof value === "number"
+                ? column.format(value)
+                : value}
+            </TableCell>
+          );
+        }
+      })}
+      <TableCell>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginLeft: "5px" }}
+          onClick={callback}
+        >
+          Test
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginLeft: "5px" }}
+        >
+          View
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          style={{ marginLeft: "5px" }}
+          startIcon={<DeleteIcon />}
+        >
+          Delete
+        </Button>
+      </TableCell>
+    </TableRow>
   );
 }
