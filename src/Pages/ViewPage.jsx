777b443ccf0d8147c23db17ceb28e91dbcf61418
Violetta Kegeles <46904779+vkegeles@users.noreply.github.com>
2021-01-21 15:02:19 +0200

    View page finished

--- src/Pages/ViewPage.jsx
+++ src/Pages/ViewPage.jsx
@@ -1,14 +1,32 @@
 import React, { useState, useEffect } from "react";
-// import { getSurveyByChildId } from "../lib/api";
+import { getSurveyByChildId } from "../lib/api";
 import Typography from "@material-ui/core/Typography";
-import { Card, CardContent, Container, Paper } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  Container,
+  Paper,
+  Toolbar,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import NavbarFull from "./../Components/NavbarFull";
+import Button from "@material-ui/core/Button";
+import TableContainer from "@material-ui/core/TableContainer";
 
 const useStyles = makeStyles({
   root: {
     width: "95%",
     margin: "auto",
+    padding: "0 20px 10px",
+  },
+  container: {
+    borderRadius: "20px",
+    maxHeight: 440,
+    padding: "0 20px 10px",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
   },
 });
 
@@ -19,14 +37,36 @@ export default function ViewPage(props) {
   let id = props.match.params.id;
 
   useEffect(() => {
-    // getSurveyByChildId(id).then((res) => setServey(res));
+    getSurveyByChildId(id).then((res) => setServey(res));
   }, [id]);
+  const getText = (survey) => {
+    console.log(survey);
+    const prc = survey.prediction_probability * 100;
+
+    if (survey.prediction == 1) {
+      return `With probability ${prc}% child has potential for ASD`;
+    } else {
+      return `With probability ${prc}% child has NO potential for ASD`;
+    }
+  };
 
   return (
     <div className="dash">
       <NavbarFull />
       <Container>
-        <Paper className={classes.root}>{JSON.stringify(survey)}</Paper>
+        <Paper className={classes.root}>
+          <TableContainer className={classes.container}>
+            <Toolbar className={classes.toolbar}>
+              <Typography variant="h6" id="tableTitle" component="div">
+                Results
+              </Typography>
+              <Button variant="contained" color="primary">
+                Send to doctor
+              </Button>
+            </Toolbar>
+          </TableContainer>
+          {survey && <Typography variant="h4">{getText(survey)}</Typography>}
+        </Paper>
       </Container>
     </div>
   );
